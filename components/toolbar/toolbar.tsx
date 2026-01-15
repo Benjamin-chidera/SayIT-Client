03c3dd23744385b042fb6ab16f3fbf91c7b8acf1
MAC <mac@MACs-MacBook-Pro-3.local>
2026-01-15 15:28:00 +0000

    refactor: changed dev url to production url

--- components/toolbar/toolbar.tsx
+++ components/toolbar/toolbar.tsx
@@ -14,7 +14,6 @@ import { useSettingsStore } from "@/store/settings.store";
 import { useCanvasStore } from "@/store/canvas.store";
 import { Send } from "../animate-ui/icons/send";
 import { AnimateIcon } from "../animate-ui/icons/icon";
-import { Button } from "../ui/button";
 
 interface ToolbarProps {
   mode: WritingMode;
@@ -30,9 +29,7 @@ const Toolbar: React.FC<ToolbarProps> = ({
   onSpeechAction,
 }) => {
   const { setOpen, language } = useSettingsStore();
-  const { uploadCanvas, uploadText, text, } = useCanvasStore();
-  // console.log(text, language);
-  
+  const { uploadCanvas, uploadText, text } = useCanvasStore();
 
   const handleUpload = () => {
     uploadCanvas(language);
@@ -41,20 +38,20 @@ const Toolbar: React.FC<ToolbarProps> = ({
   return (
     <div className="fixed z-50 bottom-4 left-1/2 -translate-x-1/2 flex gap-4 rounded-full bg-[#1a1a1a]/80 border border-gray-700/50 backdrop-blur-sm p-2 sm:p-0 sm:bg-transparent sm:border-none sm:backdrop-blur-none md:flex-col md:left-auto md:right-4 md:top-1/2 md:-translate-y-1/2 md:translate-x-0 md:mx-6 md:py-2 md:space-y-2 max-w-full md:max-h-[calc(100vh-4rem)] md:overflow-auto overflow-visible">
       {/* display only when the user uses the canvas to write */}
-      <Button
+      <IconButton
         onClick={
           mode === WritingMode.Canvas
             ? handleUpload
             : () => uploadText(language)
         }
-        // tooltip="Speak"
-        // disabled={!text}
+        tooltip="Speak"
+        disabled={!text}
       >
         <AnimateIcon animateOnHover>
           <Send />
           {text && <h1>{text}</h1>}
         </AnimateIcon>
-      </Button>
+      </IconButton>
       {/* display only when the user uses the canvas to write */}
 
       <IconButton
